a7d5a05d11b9a15050109e836d83e76b1c8e6953
Alexander <sashakuznetsov64@gmail.com>
2025-04-06 21:38:07 +0300

    2 флаг для чтения изображения

--- Laba_1/1-2.py
+++ Laba_1/1-2.py
@@ -1,7 +1,7 @@
 import cv2
 
 path = 'images/photo.jpg'
-img = cv2.imread(path, cv2.IMREAD_COLOR) # загрузка в цветном режиме BGR
+img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) # ч/б
 
 cv2.imshow('Мое окно', img)
 cv2.waitKey(0)
