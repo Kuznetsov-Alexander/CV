cee3cb87b329c4bad2ae1a848915134db48d5bd7
Alexander <sashakuznetsov64@gmail.com>
2025-04-06 21:25:20 +0300

    1 флаг для создания окна

--- Laba_1/1-2.py
+++ Laba_1/1-2.py
@@ -1,5 +1,12 @@
 import cv2
 
-img = cv2.imread('images/photo.jpg')
-cv2.imshow('output', img)
+path = 'images/photo.jpg'
+img = cv2.imread(path)
+
+cv2.namedWindow('Мое окно', cv2.WINDOW_NORMAL) #окно с изменяемым размером
+cv2.resizeWindow('Мое окно', 600, 400)
+
+cv2.imshow('Мое окно', img)
 cv2.waitKey(0)
+
+cv2.destroyAllWindows()
