f35082845026f3cf876e0d71541f64bbb45691e2
Alexander <sashakuznetsov64@gmail.com>
2025-04-06 21:40:55 +0300

    3 флаг для чтения изображения

--- Laba_1/1-2.py
+++ Laba_1/1-2.py
@@ -1,7 +1,7 @@
 import cv2
 
 path = 'images/photo.jpg'
-img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) # ч/б
+img = cv2.imread(path, cv2.IMREAD_UNCHANGED) # загружает изображение со всеми исходными данными включая прозрачность
 
 cv2.imshow('Мое окно', img)
 cv2.waitKey(0)
