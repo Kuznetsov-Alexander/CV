7b8a943488c9ceb247f4caba9ecf82b4cdde4f19
Alexander <sashakuznetsov64@gmail.com>
2025-04-10 21:31:40 +0300

    изменение размера видео

--- Laba_1/3.py
+++ Laba_1/3.py
@@ -6,11 +6,19 @@ if not cap.isOpened():
     print("Ошибка: не удалось открыть видеофайл")
     exit()
 
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) # ширина окна
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) #высота окна
+fps = cap.get(cv2.CAP_PROP_FPS) #фпс
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+
+
 while True:
     ret, frame = cap.read()
     if not(ret):
         break
-    cv2.imshow('frame', frame)
+    resized_frame = cv2.resize(frame, (width // 2, height // 10)) # изменяем размер окна
+    cv2.imshow('frame', resized_frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
